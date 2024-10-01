0a8be35efd495477a1959a9675eb906a6891a938
kazuuud <jinmakazuhiro@outlook.com>
2024-10-01 09:21:39 +0900

    0930まで

--- practice/src/main/java/com/example/demo/controller/LoginController.java
+++ practice/src/main/java/com/example/demo/controller/LoginController.java
@@ -2,16 +2,36 @@ package com.example.demo.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/")
 public class LoginController {
 
 	@GetMapping("login")
-	public String login() {
+	public String getLogin() {
 		//ビューを指定する
 		return "login";
+	}
+	
+	@PostMapping("login")
+	public String postLogin( 
+			@RequestParam("user") String username,
+			@RequestParam("pass") String password,
+			Model model ) {
+		
+		model.addAttribute("name", username );
+		model.addAttribute("pass", password );
+		
+		if(username.equals("admin") && password.equals("pass")) {
+			return "top";
+			
+		} else {
+			return "login";
+		}
 	}
 }
